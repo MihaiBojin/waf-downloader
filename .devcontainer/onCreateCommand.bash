80c9a55d7926819fc6e157304327f44a26f200ee
Mihai Bojin <mihai.bojin@gmail.com>
2024-06-26 14:12:35 +0000

    temp fix for twine

--- .devcontainer/onCreateCommand.bash
+++ .devcontainer/onCreateCommand.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-DIR=python
 
 # Create a virtualenv
-cd "$DIR"
 make create-venv
--- .devcontainer/postStartCommand.bash
+++ .devcontainer/postStartCommand.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-DIR=python
 
 # Ensure the up-to-date requirements are installed
-cd "$DIR"
 # shellcheck disable=SC1090
 eval "$(make venv)"
 make setup
