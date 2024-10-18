d631d1b82963df977fe3e55e44b8b0c81a6df22e
Mihai Bojin <mihai.bojin@gmail.com>
2024-10-18 23:47:23 +0100

    fix sed

--- scripts/helm-set-version.bash
+++ scripts/helm-set-version.bash
@@ -11,5 +11,6 @@ VERSION="$(latest_version)"
 readonly VERSION
 
 cat "$DIR"/../charts/waf-downloader/Chart.yaml.tmpl >"$DIR"/../charts/waf-downloader/Chart.yaml
-sed -i '' "s/version: \"{{ VERSION }}\"/version: $VERSION/" "$DIR"/../charts/waf-downloader/Chart.yaml
-sed -i '' "s/appVersion: \"{{ VERSION }}\"/appVersion: $VERSION/" "$DIR"/../charts/waf-downloader/Chart.yaml
+sed -i '.bak' "s/version: \"{{ VERSION }}\"/version: $VERSION/" "$DIR"/../charts/waf-downloader/Chart.yaml
+sed -i '.bak' "s/appVersion: \"{{ VERSION }}\"/appVersion: $VERSION/" "$DIR"/../charts/waf-downloader/Chart.yaml
+rm -f "$DIR"/../charts/waf-downloader/Chart.yaml.bak
