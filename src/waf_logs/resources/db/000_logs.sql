96baba03ff6f86a95acd28617158c607a3fb5362
Mihai Bojin <mihai.bojin@gmail.com>
2024-11-10 23:48:51 +0000

    zone_id

--- src/waf_logs/resources/db/000_logs.sql
+++ src/waf_logs/resources/db/000_logs.sql
@@ -1,7 +1,3 @@
-CREATE TABLE
-    IF NOT EXISTS cf_waf_logs_adaptive (
-        rayName VARCHAR(64) PRIMARY KEY,
-        "datetime" TIMESTAMP,
-        data JSONB,
-        UNIQUE (rayName, "datetime")
-    );
+create table if not exists cf_waf_logs_adaptive (rayname varchar(64) primary key, zone_id varchar(32), "datetime" timestamp, data jsonb, unique (rayname, "datetime"));
+
+CREATE INDEX IF NOT EXISTS cf_waf_logs_adaptive_zone_datetime_idx ON cf_waf_logs_adaptive (zone_id, "datetime");
