9bd52ed23864005de8bab6bf6139978d60244989
Mihai Bojin <mihai.bojin@gmail.com>
2024-11-24 23:41:28 +0000

    Added rt

--- scripts/detect-and-set-tag-version.bash
+++ scripts/detect-and-set-tag-version.bash
@@ -7,21 +7,7 @@ readonly DIR
 # shellcheck disable=SC1091
 source "$DIR/functions.bash"
 
-VERSION_FILE="$DIR/../VERSION"
-readonly VERSION_FILE
-
-VERSION="$(cat "$VERSION_FILE")"
-if [ -z "$(is_dirty)" ]; then
-    # Working dir is clean, attempt to use tag
-    GITTAG="$(get_tag_at_head)"
-
-    # If git tag found, use it
-    if [ -n "$GITTAG" ]; then
-        VERSION="$GITTAG"
-    fi
-fi
+VERSION="$(rt git::version_or_sha)"
 readonly VERSION
-
-# Output the detected tag
-echo "Updating version in '$VERSION_FILE' to: $VERSION"
+echo "Updating version in '$VERSION_FILE' to: $VERSION" >&2
 echo "$VERSION" >"$VERSION_FILE"
